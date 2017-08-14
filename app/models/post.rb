f33b0452b536ada22e2d7e85237a3a6dd5155b43
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-14 19:49:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates_uniqueness_of :title
+	validates :content, length: { minimum: 250 }
+  	validates :summary, length: { maximum: 250 }
+  	validates :category, inclusion: {:in => ["Fiction","Non-Fiction"]}
+  	validate :clickbait
+
+
+
+def clickbait
+   if title != nil && !title.start_with?('Won\'t Believe', 'Secret', 'Top', 'Guess', "You Won\'t")
+   	errors.add(:title, "Not clickbait title")
+   end
 end
+
+
+end
+
